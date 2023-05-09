ece09f629d2bc410d5dbb0c25c3c7ed44f0a2ebb
Ivan <ivanandra@gmail.com>
2023-05-09 09:29:40 -0300

    feat_make_filter_based_on_search_parameter
    finishing filters by all clinics based on multiple searchs at the same time

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,14 +1,44 @@
 const vetClinicsService = require('./vetClinicsService');
 const dentalClinicsService = require('./dentalClinicsService');
 
-const apiService = async (searchParameter) => {
-  const vetClinicsResult = await vetClinicsService(searchParameter);
-  const dentalClinicsResult = await dentalClinicsService(searchParameter);
+const apiService = async (searchParams) => {
+  const vetClinicsResult = await vetClinicsService();
+  const dentalClinicsResult = await dentalClinicsService();
+
+  console.log('busca', searchParams);
   
+  //filtering based on searchParameter
+  const vetClinicsFiltered = vetClinicsResult.filter(obj => {
+    return searchParams.some(param => {
+      for(const prop in obj){
+        if(obj.hasOwnProperty(prop) && typeof obj[prop] === 'string'){
+          if(obj[prop].toLowerCase().includes(param.toLowerCase())){
+            return true;
+          }
+        }
+      }
+      return false;
+    })
+  });
+
+  const dentalClinicsFiltered = dentalClinicsResult.filter(obj => {
+    return searchParams.some(param => {
+      for(const prop in obj){
+        if(obj.hasOwnProperty(prop) && typeof obj[prop] === 'string'){
+          if(obj[prop].toLowerCase().includes(param.toLowerCase())){
+            return true;
+          }
+        }
+      }
+      return false;
+    })
+  });
+
+
   return {
     //TODO, Query to find the results on searchParameter
-    vetClinicsResult,
-    dentalClinicsResult,
+    vetClinicsFiltered,
+    dentalClinicsFiltered,
   };
 };
 
